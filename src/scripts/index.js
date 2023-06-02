f3836492d56058631c95095c2824b91504285af4
nadyadf <nadyadf3@gmail.com>
2023-06-02 08:40:33 +0700

    Main page finish

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../scripts/responsive-navbar';
+import '../scripts/menu-list';
 
-console.log('Hello Coders! :)');
