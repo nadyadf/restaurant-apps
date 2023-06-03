5e3699cfec93442a2773986d86de8310af5f9156
nadyadf <nadyadf3@gmail.com>
2023-06-04 06:30:05 +0700

    edit path source responsie navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
-import '../scripts/responsive-navbar';
+import './views/interactivity/responsive-navbar';
 import App from './views/app';
 
 const app = new App({
