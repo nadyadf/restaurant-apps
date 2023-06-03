fcdebea594fb2ef57e80b8da482762d143e97417
nadyadf <nadyadf3@gmail.com>
2023-06-04 06:34:27 +0700

    add config for idb

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,9 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium',
+  DATABASE_NAME: 'restopedia-db',
+  DATABASE_VERSION: '1',
+  OBJECT_STORE_NAME: 'resto-favorite',
 };
 
 export default CONFIG;
